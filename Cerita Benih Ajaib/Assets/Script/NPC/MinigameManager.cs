335c38b9ec6a075c5ab16371153a2d96bbd94a52
khaldaranikhansa <khaldaranikhansae@gmail.com>
2022-12-01 13:22:19 +0700

    Add delay + fixing minigame

--- Cerita Benih Ajaib/Assets/Script/NPC/MinigameManager.cs
+++ Cerita Benih Ajaib/Assets/Script/NPC/MinigameManager.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MinigameManager : MonoBehaviour
 {
     public List<QuestionNPC> questionList = new List<QuestionNPC>();
     public TMP_Text question, OptionA, OptionB, OptionC, OptionD, indikator;
-    //optionE;
     public Image imageQuest;
     public GameObject feedbackBenar, feedbackSalah;
+    public GameObject winPanel, retryPanel;
     private int indexQuestion;
+    private int trueResult = 0;
+    private bool endQuestion = false;
 
-    
+
     private void Start() 
     {
         SetupQuestion();
@@ -22,13 +25,15 @@ public class MinigameManager : MonoBehaviour
 
     void SetupQuestion()
     {
+
         question.text = questionList[indexQuestion].question;
-        indikator.text = questionList[indexQuestion].indikator;
 
         OptionA.text = questionList[indexQuestion].OptionA;
         OptionB.text = questionList[indexQuestion].OptionB;
         OptionC.text = questionList[indexQuestion].OptionC;
         OptionD.text = questionList[indexQuestion].OptionD;
+
+        indikator.text = questionList[indexQuestion].indikator;
 
         if(questionList[indexQuestion].imageQuest != null)
         {
@@ -38,14 +43,14 @@ public class MinigameManager : MonoBehaviour
         else
         {
             imageQuest.gameObject.SetActive(false);
-        }
+        }        
     }
 
     public void ButtonQuestion(int n)
     {
         if(n == questionList[indexQuestion].result)
         {
-            questionResult(true);
+            questionResult(true);           
         }
         else
         {
@@ -55,11 +60,13 @@ public class MinigameManager : MonoBehaviour
 
     void questionResult(bool resultTrue)
     {
-        if(resultTrue)
+        if (resultTrue)
         {
             Debug.Log("Benar");
             feedbackBenar.SetActive(false);
             feedbackBenar.SetActive(true);
+
+            trueResult++;
         }
         else
         {
@@ -68,7 +75,49 @@ public class MinigameManager : MonoBehaviour
             feedbackSalah.SetActive(true);
         }
 
-        indexQuestion++;
-        SetupQuestion();
+
+        if (indexQuestion != 2)
+        {
+            indexQuestion++;
+            SetupQuestion();
+        }
+        else if (indexQuestion == 2)
+        {
+            endQuestion = true;
+        }
+    }
+
+    public void Update()
+    {
+        StartCoroutine(ConditionPanel());
+        
+        //delay
+        IEnumerator ConditionPanel()
+        {
+            if (trueResult >= 2 && endQuestion == true)
+        {
+            yield return new WaitForSeconds(1);
+            winPanel.SetActive(true);
+        }
+        if (trueResult <= 1 && endQuestion == true)
+        {
+            yield return new WaitForSeconds(1);
+            retryPanel.SetActive(true);
+
+        }
+    }
+}
+
+    public void RetryKakFlora()
+    {
+        SceneManager.LoadScene("KakFlora");
+    }
+    public void RetryKakSandi()
+    {
+        SceneManager.LoadScene("KakSandi");
+    }
+    public void RetryPakDali()
+    {
+        SceneManager.LoadScene("PakDali");
     }
 }
