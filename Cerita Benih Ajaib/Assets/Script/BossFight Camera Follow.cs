090707541248574196e6dead38c79d18f87f972d
renzien <alifrizki1011@gmail.com>
2022-12-01 14:03:52 +0700

    update : boss fight camera follow

---
+++ Cerita Benih Ajaib/Assets/Script/BossFight Camera Follow.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFightCameraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject soal;
+    private Vector3 offset = new Vector3(0, 0, -10);
+    private Vector3 velocity = Vector3.zero;
+    private float smoothTime = 0.25f;
+    public float timer = 0;
+
+    [SerializeField] private Transform target;
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        // Camera Panning Boss
+        if (timer >= 6)
+        {
+            Vector3 targetPosition = target.position + offset;
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+            soal.SetActive(true);
+        }
+    }
+}
